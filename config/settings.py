6c0466d3e97eda7f93b002f3cb22495c6b9cecbe
Anuj-kushwaha664 <anujkushwaha664@gmail.com>
2023-09-13 00:11:02 +0530

    allowed hosts updated

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ij*)*g=&@tts_4m=k$$3xsgcd(q+6j=_lpj%6p^)px3md+n-ig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
